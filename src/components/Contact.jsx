f8a51b9e6cbaaf18552ea9b938eb720e4bc6944b
Thales7 <sangarekabinet7@gmail.com>
2026-07-28 00:31:36 +0200

    update formulaire

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { company } from "../data/content";
 import Icon from "./Icon";
 import emailjs from "@emailjs/browser";
